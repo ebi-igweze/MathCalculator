0b126906a526010694fe7f3a78f450d02dfb324a
ebi igweze <ebi.igweze@gmail.com>
2018-02-13 04:30:50 +0100

    changes MathCalculator members to static members, removes instatiation from Program.cs

--- MathCalculator.cs
+++ MathCalculator.cs
@@ -4,22 +4,22 @@ namespace Calculator
 {
     public class MathCalculator 
     {
-        public int Add(int param1, int param2) 
+        public static int Add(int param1, int param2) 
         {
             return param1 + param2;
         }
 
-        public int Subtract(int param1, int param2) 
+        public static int Subtract(int param1, int param2) 
         {
             return param1 - param2;
         }
 
-        public int Multiply(int param1, int param2) 
+        public static int Multiply(int param1, int param2) 
         {
             return param1 * param2;
         }
 
-        public int Divide(int numerator, int denominator) 
+        public static int Divide(int numerator, int denominator) 
         {
             if (denominator == 0) throw new InvalidOperationException("Cannot Divide by O");
             return numerator / denominator;
--- Program.cs
+++ Program.cs
@@ -10,20 +10,20 @@ namespace Calculator
             int secondParam = 3;
 
             // MathCalculator Object 
-            MathCalculator calculator = new MathCalculator();
+            //MathCalculator calculator = new MathCalculator();
 
-            int addOperation = calculator.Add(firstParam, secondParam);
+            int addOperation = MathCalculator.Add(firstParam, secondParam);
             // Writing Addition to Console
             Console.WriteLine($"Adding {firstParam} and {secondParam} results in: {addOperation}");
 
-            int subtractOperation = calculator.Subtract(firstParam, secondParam);
+            int subtractOperation = MathCalculator.Subtract(firstParam, secondParam);
             // Writing Subtraction to Console
             Console.WriteLine($"Subtracting {secondParam} from {firstParam} results in: {subtractOperation}");
 
-            int multiplyOperation = calculator.Multiply(firstParam, secondParam);
+            int multiplyOperation = MathCalculator.Multiply(firstParam, secondParam);
             Console.WriteLine($"Multiplying {firstParam} and {secondParam} results in: {multiplyOperation}");
 
-            int divideOperation = calculator.Divide(firstParam, secondParam);
+            int divideOperation = MathCalculator.Divide(firstParam, secondParam);
             Console.WriteLine($"Divide {firstParam} by {secondParam} results in: {divideOperation}");
         }
     }
